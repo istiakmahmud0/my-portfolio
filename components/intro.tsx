6a9cc0cbeb34fbffd1d479fbbe25a5b0768452d2
istiakmahmud0 <71057614+istiakmahmud0@users.noreply.github.com>
2023-09-05 00:27:37 +0600

    feat: added bio

--- components/intro.tsx
+++ components/intro.tsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 
 const Intro = () => {
   return (
-    <section>
+    <section className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]">
       <div className="flex items-center justify-center relative">
         <motion.div
           initial={{ opacity: 0, scale: 0 }}
@@ -22,6 +22,17 @@ const Intro = () => {
           />
         </motion.div>
       </div>
+      <motion.h1
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <span className="font-bold">Hello, I'm Ricardo.</span> I'm a{" "}
+        <span className="font-bold">full-stack developer</span> with{" "}
+        <span className="font-bold">8 years</span> of experience. I enjoy
+        building <span className="italic">sites & apps</span>. My focus is{" "}
+        <span className="underline">React (Next.js)</span>.
+      </motion.h1>
     </section>
   );
 };
