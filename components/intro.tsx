f78bb2c3ae589a629f8e7a6960a8eea425a0070b
istiakmahmud0 <71057614+istiakmahmud0@users.noreply.github.com>
2023-09-08 23:57:51 +0600

    wip: intro section

--- components/intro.tsx
+++ components/intro.tsx
@@ -2,6 +2,10 @@
 import Image from "next/image";
 import React from "react";
 import { motion } from "framer-motion";
+import Link from "next/link";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import { FaGithubSquare } from "react-icons/fa";
 
 const Intro = () => {
   return (
@@ -27,12 +31,46 @@ const Intro = () => {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Ricardo.</span> I'm a{" "}
+        <span className="font-bold">Hello, I'm Mahmud.</span> I'm a{" "}
         <span className="font-bold">full-stack developer</span> with{" "}
-        <span className="font-bold">8 years</span> of experience. I enjoy
+        <span className="font-bold">4 years</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
         <span className="underline">React (Next.js)</span>.
       </motion.h1>
+      <motion.div
+        className="flex gap-2 justify-center flex-col sm:flex-row text-lg font-medium"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.1,
+        }}
+      >
+        <Link
+          href="#contact"
+          className="bg-gray-900 text-white px-7 py-3 rounded-full flex gap-2 items-center"
+        >
+          Contact me here
+          <BsArrowRight />
+        </Link>
+        <a
+          href=""
+          className="bg-white px-7 py-3 rounded-full flex items-center"
+        >
+          Download CV <HiDownload />
+        </a>
+        <a
+          href=""
+          className="bg-white p-4 rounded-full flex items-center gap-2"
+        >
+          <BsLinkedin />
+        </a>
+        <a
+          href=""
+          className="bg-white p-4 rounded-full flex items-center gap-2"
+        >
+          <FaGithubSquare />
+        </a>
+      </motion.div>
     </section>
   );
 };
