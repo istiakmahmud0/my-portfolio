db9ebc28f49c99581d80b1e902fc64d8af7807f5
istiakmahmud0 <71057614+istiakmahmud0@users.noreply.github.com>
2023-09-24 10:23:08 +0600

    wip : project section

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import SectionHeading from "./SectionHeading";
 import { projectsData } from "@/lib/data";
+import Image from "next/image";
 
 const Projects = () => {
   return (
@@ -9,7 +10,7 @@ const Projects = () => {
       <div>
         {projectsData.map((project, index) => (
           <React.Fragment key={index}>
-            <Project />
+            <Project {...project} />
           </React.Fragment>
         ))}
       </div>
@@ -19,6 +20,19 @@ const Projects = () => {
 
 export default Projects;
 
-const Project = () => {
-  return <div></div>;
+type projectDataProps = (typeof projectsData)[number];
+
+const Project = ({ title, description, tags, imageUrl }: projectDataProps) => {
+  return (
+    <section>
+      <h3>{title}</h3>
+      <p>{description}</p>
+      <ul>
+        {tags.map((tag, index) => (
+          <li key={index}>{tag}</li>
+        ))}
+      </ul>
+      <Image src={imageUrl} width={`200`} height={`200`} alt="" />
+    </section>
+  );
 };
